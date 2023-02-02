b2410c22ce8b0839fd7edc089bfe3a0d30443024
Yous-eef <115256728+Yous-eef@users.noreply.github.com>
2023-02-02 18:52:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var daynum = prompt("Enter Number")
+
+switch(daynum){
+    case '1' : alert(`Saturday`)
+    break;
+    case '2' : alert(`Sunday`)
+    break;
+    case '3' : alert(`Monday`)
+    break;
+    case '4' : alert(`Tuesday`)
+    break;
+    case '5' : alert(`Wednesday`)
+    break;
+    case '6' : alert(`Thursday`)
+    break;
+    case '7' : alert(`Friday`)
+    break;
+    default : alert(`Enter invalid number`)
+}
